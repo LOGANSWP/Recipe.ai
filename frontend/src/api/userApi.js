9333bbd301f76996d5a97f132d17d0386c2cd0b8
Weipeng Su <logansu1025@outlook.com>
2025-12-03 23:58:37 -0600

    Adjust code with authentication

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -4,6 +4,30 @@ const getMyUser = () => {
   return api.get(`/user/my/user`);
 };
 
+const fetchAllIngredients = async () => {
+  return await api.get(`/inventory/ingredients`)
+}
+
+const fetchAllKitchenware = async () => {
+  return await api.get(`/inventory/kitchenware`)
+}
+
+const addOrUpdateInventory = async (method, url, payload) => {
+  if (method === "POST") {
+    return await api.post(url, payload)
+  } else if (method === "PUT") {
+    return await api.put(url, payload)
+  }
+}
+
+const deleteInventory = async (url) => {
+  return api.delete(url)
+}
+
 export {
   getMyUser,
+  fetchAllIngredients,
+  fetchAllKitchenware,
+  addOrUpdateInventory,
+  deleteInventory,
 };
